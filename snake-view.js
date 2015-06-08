201562c151e02441162c53554824742cf8d91fad
Emmanuel Genard <edgenard@gmail.com>
2015-06-08 11:34:09 -0400

    Style the game

--- snake-view.js
+++ snake-view.js
@@ -28,12 +28,13 @@
   view.prototype.stopPlaying = function () {
     clearInterval(this.keepPlaying);
     clearInterval(this.appleMove);
-    $(".snake-score-1 h3").html(this.board.snake1.score);
-    this.board.snake2 && $(".snake-score-2 h3").html(this.board.snake2.score);
-    this.$el.find(".end-screen").css("display", "block");
+    $(".snake-score-1 h3").empty().html(this.board.snake1.score);
+    this.board.snake2 && $(".snake-score-2 h3").empty().html(this.board.snake2.score);
+    this.$el.find(".end-screen").css("display", "flex");
     var score = this.board.highScore();
     var winner = this.board.winner();
-    if(winner) $(".end-screen h2").after("<h3>The winner is " + winner+"</h3>");
+    $(".end-screen .winner").empty();
+    if(winner) $(".end-screen .winner").html("The winner is " + winner);
     $(".end-screen .winning-score").text("The high score is " + score);
     
     
