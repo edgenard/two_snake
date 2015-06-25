8c37f4ad5ce2951ab3a8782885f091e8b1230099
Emmanuel Genard <edgenard@gmail.com>
2015-06-25 19:13:01 -0400

    Add jasmine-jquery plugin, add fixture

--- tests/spec/BoardSpec.js
+++ tests/spec/BoardSpec.js
@@ -7,16 +7,24 @@ describe("Board", function () {
     expect(board2.players).toEqual(2);
   });
   // I think I should get rid of the rendering of ui in the render function and just test that the grid is reflecting the game state
-  
+  beforeEach(function () {
+    jasmine.getFixtures().fixturesPath = "../tests/fixtures/";
+    loadFixtures("board.html");
+  });
   it("renders the board correctly");
   
   it("keep snakes visible");
   
   it("sets the apple");
   
-  it("checks that a snake has not eaten itself");
+  it("checks that a snake has not eaten itself", function () {
+    var board = new Snake.board(1);
+    spyOn(board, "checkSnakes");
+    board.render($(".snake-board"));
+    expect(board.checkSnakes()).toHaveBeenCalled();
+  });
   
-  it("checks that a snake has not the other snake");
+  it("checks that a snake has not eaten the other snake");
   
   it("stops rendering when one of the snakes has died");
   
