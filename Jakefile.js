5fad2d488f6eb8b45d3de57aaf6ae81ae67725dc
Emmanuel Genard <edgenard@gmail.com>
2015-10-14 11:26:43 -0400

    INstalled semver

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,26 @@
 (function() {
   'use strict';
 
+  var semver = require("semver");
 
   desc("Default Talks");
-  task("default", function () {
+  task("default",["check-node"], function () {
 
     console.log("\n\nBuild OK");
   });
+
+  desc("Checking Node Version");
+  task("check-node", function () {
+  console.log("Checking Node version: .");
+
+  var packageJSON = require("./package.json");
+  var expectedVersion = "v"+packageJSON.engines.node;
+
+  var actualVersion = process.version;
+  if(semver.neq(expectedVersion, actualVersion)) {
+    fail("Incorrect Node Version. Expected: " + expectedVersion  + " Actual: " + actualVersion);
+  }
+});
+
+
 }());
