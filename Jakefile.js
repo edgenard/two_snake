f5f2c1d4e579e18d5e7fe9a94f0754f7385331f6
Emmanuel Genard <edgenard@gmail.com>
2015-10-14 11:37:30 -0400

    Added lint task

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,11 @@
+/* globals desc: false, task: false, complete: false, fail: false, jake:false */
+
 (function() {
   'use strict';
 
   var semver = require("semver");
+  var jshint = require("simplebuild-jshint");
+
 
   desc("Default Talks");
   task("default",["check-node"], function () {
@@ -21,6 +25,52 @@
     fail("Incorrect Node Version. Expected: " + expectedVersion  + " Actual: " + actualVersion);
   }
 });
+
+
+desc("Run JShint");
+task("lint", function () {
+  process.stdout.write("Linting JavaScript: ");
+
+  jshint.checkFiles({
+    files: ["Jakefile.js", "src/**/*.js"],
+    options: lintOptions(),
+
+    globals: lintGlobals()
+
+  }, complete, fail);
+
+}, {async: true});
+
+
+function lintOptions() {
+  return {
+    bitwise  : true,
+    eqeqeq   : true,
+    forin    : true,
+    freeze   : true,
+    latedef  : "nofunc",
+    noarg    : true,
+    nocomma  : true,
+    nonbsp   : true,
+    nonew    : true,
+    strict   : true,
+    undef    : true,
+    node     : true,
+    browser  : true,
+  };
+}
+
+function lintGlobals() {
+  return {
+    describe   : false,
+    it         : false,
+    after      : false,
+    before     : false,
+    beforeEach : false,
+    afterEach  : false,
+    chai       : false,
+  };
+}
 
 
 }());
