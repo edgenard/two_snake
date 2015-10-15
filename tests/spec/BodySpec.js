2ec51a49167eff81e7a0d7e5ece648e656f7a71a
Emmanuel Genard <edgenard@gmail.com>
2015-10-15 10:43:16 -0400

    attributes and movement features added to body

--- tests/spec/BodySpec.js
+++ tests/spec/BodySpec.js
@@ -6,9 +6,64 @@ describe("Body", function () {
       dir: "E"
     });
 
-    expect(body.segments).toEqual([[0,0], [0,1]]);
-    expect(body.color).toBe("black");
-    expect(body.dir).toBe("E");
+    expect(body.getSegments()).toEqual([[0,0], [0,1]]);
+    expect(body.getColor()).toBe("black");
+    expect(body.getDir()).toBe("E");
+  });
+  beforeEach(function () {
+    this.body = new Snake.Body({
+      segments: [[0,0], [0,1]],
+      color: "black",
+      dir: "E"
+    });
+
+  });
+
+  it("grows", function () {
+
+    this.body.grow();
+
+    expect(this.body.getSegments()).toEqual([[0,0], [0,1], [0,2]]);
+
+  });
+
+  it("grows in the right direction", function () {
+    this.body._dir = "S";
+
+    this.body.grow();
+
+    expect(this.body.getSegments()).toEqual([[0,0], [0,1], [1,1]]);
+  });
+
+  it("moves east", function () {
+
+    this.body.move();
+
+    expect(this.body.getSegments()).toEqual([[0,1],[0,2]]);
+  });
+
+  it("moves west", function () {
+    this.body._dir = "W";
+
+    this.body.move();
+
+    expect(this.body.getSegments()).toEqual([[0,1], [0,0]]);
+  });
+
+  it("moves south", function () {
+    this.body._dir = "S";
+
+    this.body.move();
+
+    expect(this.body.getSegments()).toEqual([[0, 1], [1,1]]);
+  });
+
+  it("moves north", function () {
+    this.body._dir = "N";
+
+    this.body.move();
+
+    expect(this.body.getSegments()).toEqual([[0,1], [-1,1]]);
   });
 
 });
